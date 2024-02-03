1c21992d84c68fa23bc6140bdb487a7af05d6bdd
Demetrios Vlassis <dimitrios.v.2002@gmail.com>
2024-02-03 14:30:27 +0200

    Implement LevelState into RenderLevel

--- src/components/level-layout/RenderLevel.tsx
+++ src/components/level-layout/RenderLevel.tsx
@@ -3,61 +3,36 @@ import Sprite from "../Sprite";
 import styles from "./RenderLevel.module.css";
 import LevelBackgroundTilesLayer from "./LevelBackgroundTilesLayer";
 import LevelPlacementsLayer from "./LevelPlacementsLayer";
+import { useEffect, useState } from "react";
+import { LevelState } from "@/classes/LevelState";
+import { ILevel } from "@/interfaces/ILevel.interface";
 
 interface IRenderLevelProps {}
 
 const RenderLevel: React.FC<IRenderLevelProps> = () => {
-  const level = {
-    theme: LEVEL_THEMES.BLUE,
-    tilesWidth: 8,
-    tilesHeight: 8,
-    placements: [
-      // "Level 0"
-      // { id: 0, x: 0, y: 0, frameCoord: "0x2" },
-      // { id: 1, x: 1, y: 1, frameCoord: "0x2" },
-      // { id: 2, x: 2, y: 2, frameCoord: "0x2" },
-      // { id: 3, x: 3, y: 3, frameCoord: "0x2" },
-      // { id: 4, x: 4, y: 4, frameCoord: "0x2" },
-      // { id: 5, x: 5, y: 5, frameCoord: "0x2" },
-      // { id: 6, x: 6, y: 6, frameCoord: "0x2" },
-      // { id: 7, x: 7, y: 7, frameCoord: "0x2" },
+  const [level, setLevel] = useState<ILevel>();
 
-      // Level '1'
-      // { id: 0, x: 0, y: 0, frameCoord: "0x2" },
-      // { id: 1, x: 4, y: 2, frameCoord: "0x2" },
-      // { id: 2, x: 2, y: 2, frameCoord: "0x2" },
-      // { id: 3, x: 7, y: 3, frameCoord: "0x2" },
-      // { id: 4, x: 2, y: 9, frameCoord: "0x2" },
-      // { id: 5, x: 3, y: 5, frameCoord: "0x2" },
-      // { id: 6, x: 1, y: 6, frameCoord: "0x2" },
-      // { id: 7, x: 7, y: 7, frameCoord: "0x2" },
-      // { id: 8, x: 9, y: 7, frameCoord: "0x2" },
+  useEffect(() => {
+    const levelState = new LevelState("1-1", (newState: ILevel) => {
+      setLevel(newState);
+    });
 
-      //Level '2'
-      { id: 0, x: 2, y: 2, frameCoord: "0x2" },
-      { id: 1, x: 2, y: 4, frameCoord: "0x2" },
-      { id: 2, x: 2, y: 6, frameCoord: "0x2" },
-      { id: 3, x: 2, y: 8, frameCoord: "0x2" },
-      { id: 4, x: 4, y: 2, frameCoord: "0x2" },
-      { id: 5, x: 4, y: 4, frameCoord: "0x2" },
-      { id: 6, x: 4, y: 6, frameCoord: "0x2" },
-      { id: 7, x: 4, y: 8, frameCoord: "0x2" },
-      { id: 8, x: 6, y: 2, frameCoord: "0x2" },
-      { id: 9, x: 6, y: 4, frameCoord: "0x2" },
-      { id: 10, x: 6, y: 6, frameCoord: "0x2" },
-      { id: 11, x: 6, y: 8, frameCoord: "0x2" },
-      { id: 12, x: 8, y: 2, frameCoord: "0x2" },
-      { id: 13, x: 8, y: 4, frameCoord: "0x2" },
-      { id: 14, x: 8, y: 6, frameCoord: "0x2" },
-      { id: 15, x: 8, y: 8, frameCoord: "0x2" },
-      { id: 16, x: 7, y: 8, frameCoord: "0x2" },
-    ],
-  };
+    setLevel(levelState.getState());
+
+    return () => {
+      levelState.destroy();
+    };
+  }, []);
+
+  if (!level) {
+    return null;
+  }
+
   return (
     <div
       className={styles.fullScreenContainer}
       style={{
-        background: THEME_BACKGROUNDS[level.theme],
+        background: THEME_BACKGROUNDS[level.theme!],
       }}
     >
       <div className={styles.gameScreen}>
